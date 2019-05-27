2d907b2c4144c1c40d87d7b485cffe738221ec97
kfincher <38328487+kfincher@users.noreply.github.com>
2019-05-26 18:08:48 -0700

    Initial Push

---
+++ CMPM163 Homework3/Assets/meatballScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meatballScript : MonoBehaviour
+{
+    AudioSource aud;
+    bool played = false;
+    float buffer = 2.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > buffer && played == false) {
+            played = true;
+            aud.Play();
+        } 
+    }
+}
---
+++ CMPM163 Homework3/Assets/sliderVolume.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sliderVolume : MonoBehaviour
+{
+    Slider slider;
+    public AudioSource audioM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.value = audioM.volume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioM.volume = slider.value;
+    }
+}
